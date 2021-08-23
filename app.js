1213f9e83c2ba3740f1e0f0d60164af5007e4053
PrakashKunal <87089252+PrakashKunal@users.noreply.github.com>
2021-08-23 18:41:56 +0530

    update-Added logic to update and display scores

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var P2button=document.querySelector('#P2button');
 var reset=document.querySelector('#reset');
 var p1scr=0;
 var p2scr=0;
+var crntscr;
 P1button.addEventListener('click',function()
 {
-    console.log('Clicked P1');
+    ++p1scr;
+    document.querySelector('#P1score').innerText=p1scr;
 })
 P2button.addEventListener('click',function()
 {
-    console.log('Clicked P2');
+    ++p2scr;
+    document.querySelector('#P2score').innerText=p2scr;
 })
 reset.addEventListener('click',function()
 {
