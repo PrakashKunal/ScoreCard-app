c6e0eee2ea6fffe7efbf4970db3d8c99fb7dbbbb
PrakashKunal <87089252+PrakashKunal@users.noreply.github.com>
2021-08-23 18:22:21 +0530

    Feat-Added javascript, some style and logic

---
+++ app.js
@@ -0,0 +1,17 @@
+var P1button=document.querySelector('#P1button');
+var P2button=document.querySelector('#P2button');
+var reset=document.querySelector('#reset');
+var p1scr=0;
+var p2scr=0;
+P1button.addEventListener('click',function()
+{
+    console.log('Clicked P1');
+})
+P2button.addEventListener('click',function()
+{
+    console.log('Clicked P2');
+})
+reset.addEventListener('click',function()
+{
+    console.log('Clicked Reset');
+})
